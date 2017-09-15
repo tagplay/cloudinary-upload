dbe8297a90e08aeb2ec19e934a4bd1a781381caf
Fannar Snær <fannar@lambagras.is>
2017-09-15 14:28:25 +0000

    Initial code

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var FormData = require('form-data');
+var Wreck = require('wreck');
+var config = require('prefect-worker-config');
+var sign = require('@tagplay/cloudinary-signature');
+
+if (!config || !config.cloudinary) {
+  throw new Error('Missing config for Cloudinary');
+}
+var cloudinary = config.cloudinary;
+
+var log = config.log;
+
+module.exports = upload;
+
+function upload(file, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+  var url = [
+    'https://api.cloudinary.com',
+    'v1_1',
+    cloudinary.cloud_name,
+    'auto',
+    'upload'
+  ].join('/');
+  var signedParams = sign(cloudinary.api_key, cloudinary.api_secret, options);
+  log.debug({ url: url, params: signedParams }, 'Cloudinary data');
+
+  var form = new FormData();
+  form.append('file', file);
+  Object.keys(signedParams).forEach(function(key) {
+    if (key === 'tags' && typeof signedParams[key] !== 'string') {
+      form.append('tags', signedParams[key].sort().join(','));
+    } else {
+      form.append(key, signedParams[key]);
+    }
+  });
+
+  Wreck.post(
+    url,
+    {
+      headers: form.getHeaders(),
+      payload: form
+    },
+    function(err, res, payload) {
+      if (err) log.debug({ err: err }, 'Problem requesting');
+      if (payload) payload = JSON.parse(payload);
+      cb(err, payload);
+    }
+  );
+}
